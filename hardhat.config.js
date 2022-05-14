0fc09320e1c862ad98ee9a8e2ee0f73cd6f04e17
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2022-05-14 20:10:04 +0530

    initiliazed hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers")
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.4",
+  networks : {
+    goerli : {
+      url : process.env.GOERLI_URL,
+      accounts : [process.env.PRIVATE_KEY],
+    }
+  }
+}
